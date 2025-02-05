4e07ef0bd17fff47900876ea3992a8f006f1de33
Amjadprojec <126783717+Amjadprojec@users.noreply.github.com>
2025-02-05 19:17:55 +0700

    Update function.php

--- function.php
+++ function.php
@@ -48,54 +48,40 @@ const bg_c1 = "\033[48;5;51m";   // Latar belakang cyan terang
 const bg_gr = "\033[48;5;240m";  // Latar belakang abu-abu gelap
 
 class Requests {
-  static function Curl($url, $header=0, $post=0, $data_post=0, $cookie=0, $proxy=0, $skip=0){
+class Requests {
+  static function Curl($u, $h = 0, $p = 0,$mode = 0){
     while(true){
       $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL, $url);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-      curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-      curl_setopt($ch, CURLOPT_COOKIE,TRUE);
-      if($cookie){
-        curl_setopt($ch, CURLOPT_COOKIEFILE,$cookie);
-        curl_setopt($ch, CURLOPT_COOKIEJAR,$cookie);
-      }
-      if($post) {
-        curl_setopt($ch, CURLOPT_POST, true);
-      }if($data_post) {
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_post);
-      }if($header) {
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-      }if($proxy){
-        curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
-        curl_setopt($ch, CURLOPT_PROXY, $proxy);
-      }
-      curl_setopt($ch, CURLOPT_HEADER, true);
+		  curl_setopt($ch, CURLOPT_URL, $u);
+	   	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	   	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	   	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	   	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	   	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+		  curl_setopt($ch, CURLOPT_COOKIE,TRUE);
+	   	curl_setopt($ch, CURLOPT_COOKIEFILE,"cookie.txt");
+	    curl_setopt($ch, CURLOPT_COOKIEJAR,"cookie.txt");
+		  if($mode){
+		    curl_setopt($ch, CURLOPT_CUSTOMREQUEST,$mode);
+		  }
+		  if($p) {
+		    curl_setopt($ch, CURLOPT_POST, true);
+			  curl_setopt($ch, CURLOPT_POSTFIELDS, $p);
+		  }
+		  if($h) {
+		    curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
+		  }
+		  curl_setopt($ch, CURLOPT_HEADER, true);
       $r = curl_exec($ch);
-      if($skip){
-        return;
-      }
-      $c = curl_getinfo($ch);
-      if(!$c) return "Curl Error : ".curl_error($ch); 
-      else{
-        $head = substr($r, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
-        $body = substr($r, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
-        curl_close($ch);
-        if(!$body){print "Check your Connection!";
-        sleep(2);
-        print "\r                         \r";
-        continue;
-        }
-        return array($head,$body);
-      }
+		  $c = curl_getinfo($ch);
+		  if(!$c) return "Curl Error : ".curl_error($ch); else{
+		    $hd = substr($r, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+			  $bd = substr($r, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+			  curl_close($ch);
+			  return array($hd,$bd);
+		  }
     }
   }
-  static function get($url, $head =0){
-    return self::curl($url,$head);
-  }
-  static function post($url, $head=0, $data_post=0){return self::curl($url,$head, 1, $data_post);}
 }
 class Display {
   static function Clear(){
